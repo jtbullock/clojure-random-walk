5a3e69df30226083646e868cdc9a52e455883cc4
Josh Bullock <mail@joshb.me>
2022-10-12 11:08:53 -0400

    Create simulation and add fixed point

--- src/clojure_random_walk/core.clj
+++ src/clojure_random_walk/core.clj
@@ -8,5 +8,11 @@
 (defn create-simulation 
   "Create a new simluation with the specified `width` and `height`"
   [width height]
-  {:width width, :height height, :points [] }
+  {:width width, :height height, :fixed-points #{} }
+)
+
+(defn add-fixed-point
+  "Add a fixed point to a simulation"
+  [simulation x y]
+  (update simulation :fixed-points conj [x y])
 )
--- test/clojure_random_walk/core_test.clj
+++ test/clojure_random_walk/core_test.clj
@@ -9,8 +9,12 @@
 (deftest create-simulation-test
   (testing "create-simulation returns a new simulation with the provided
             width and height"
-    (is
-     (let [width 200 height 200]
-      (= {:width width :height height :points [] } 
-         ( create-simulation width height ))))))
+    (is (let [width 200 height 200]
+          (= {:width width :height height :fixed-points #{} } 
+             ( create-simulation width height ))))))
 
+(deftest add-fixed-point-test
+  (testing "add-fixed-point adds a new point to simulation"
+    (is (let [simulation (create-simulation 200 200)]
+          (= {:width 200 :height 200 :fixed-points #{[50,50]}}
+             ( add-fixed-point simulation 50 50 ))))))

d32afe3c02eb0aa31a9b245928ae4b46d791e424
Josh Bullock <mail@joshb.me>
2022-10-12 11:40:07 -0400

    Check if points are adjacent

--- src/clojure_random_walk/core.clj
+++ src/clojure_random_walk/core.clj
@@ -12,7 +12,18 @@
 )
 
 (defn add-fixed-point
-  "Add a fixed point to a simulation"
+  "Add a fixed point to a `simulation` at coordinates `x` and `y`"
   [simulation x y]
   (update simulation :fixed-points conj [x y])
+)
+
+(defn are-points-adjacent?
+  "Determine if coordinates `x` and `y` are adjacent to a fixed point
+   with coordinates `fixed-x fixed-y`"
+  [[fixed-x fixed-y] [x y]]
+  (let [x-delta (abs (- x fixed-x))
+        y-delta (abs (- y fixed-y))]
+    (or (and (= 0 x-delta) (= 1 y-delta))
+        (and (= 1 x-delta) (= 0 y-delta)))
+  )
 )
--- test/clojure_random_walk/core_test.clj
+++ test/clojure_random_walk/core_test.clj
@@ -2,19 +2,43 @@
   (:require [clojure.test :refer :all]
             [clojure-random-walk.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
-
 (deftest create-simulation-test
   (testing "create-simulation returns a new simulation with the provided
-            width and height"
+            width and height."
     (is (let [width 200 height 200]
-          (= {:width width :height height :fixed-points #{} } 
-             ( create-simulation width height ))))))
+          (= {:width width :height height :fixed-points #{}} 
+             ( create-simulation width height))))))
 
 (deftest add-fixed-point-test
-  (testing "add-fixed-point adds a new point to simulation"
+  (testing "add-fixed-point adds a new point to simulation."
     (is (let [simulation (create-simulation 200 200)]
           (= {:width 200 :height 200 :fixed-points #{[50,50]}}
-             ( add-fixed-point simulation 50 50 ))))))
+             ( add-fixed-point simulation 50 50))))))
+
+(deftest are-points-adjacent?-test
+  (let [fixed-point [50 50]]
+    (testing "When no point is next to coordinates, returns false."
+      (is (= false (are-points-adjacent? fixed-point [5 5]))))
+    
+    (testing "When fixed point is directly above coordinates, returns true."
+      (is (= true (are-points-adjacent? fixed-point [50 51])))) 
+  
+    (testing "When fixed point is directly below coordinates, returns true."
+      (is (= true (are-points-adjacent? fixed-point [50 49]))))
+    
+    (testing "When fixed point is directly to the left, returns true."
+      (is (= true (are-points-adjacent? fixed-point [51 50]))))
+    
+    (testing "When fixed point is directly to the right, returns true."
+      (is (= true (are-points-adjacent? fixed-point [49 50]))))
+    
+    (testing "When fixed point is above and to the left, returns false."
+      (is (= false (are-points-adjacent? fixed-point [51 51]))))
+    
+    (testing "When fixed point is at x+1 but y+5, returns false."
+      (is (= false (are-points-adjacent? fixed-point [51 55]))))
+    
+    (testing "When fixed point is at y+1 but x+5, returns false."
+      (is (= false (are-points-adjacent? fixed-point [55 51]))))
+  )
+)

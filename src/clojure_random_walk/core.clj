468644cfa7ba4c64da468bea141b2572ed4ee431
Josh Bullock <mail@joshb.me>
2022-11-03 10:18:17 -0400

    Set up lein run

--- src/clojure_random_walk/core.clj
+++ src/clojure_random_walk/core.clj
@@ -1,3 +1,8 @@
-(ns clojure-random-walk.core)
+(ns clojure-random-walk.core
+  (:require [clojure-random-walk.simulation :refer [run-simulation]]
+            [clojure-random-walk.text-output :refer [print-simulation-to-console]]))
 
-
+(defn -main [& args]
+  (do
+    (println "Running simulation...")
+    (print-simulation-to-console (run-simulation))))
--- test/clojure_random_walk/core_test.clj
+++
@@ -1,4 +0,0 @@
-(ns clojure-random-walk.core-test
-  (:require [clojure.test :refer :all]
-            [clojure-random-walk.core :refer :all]))
-

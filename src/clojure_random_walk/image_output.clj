e2cf9decc1846f7275f85590a8f0c74e53f8b32b
Josh Bullock <mail@joshb.me>
2022-11-15 11:10:39 -0500

    Use lisp-case naming convention

--- src/clojure_random_walk/image_output.clj
+++ src/clojure_random_walk/image_output.clj
@@ -6,24 +6,24 @@
 
 (defn create-image [w h] (BufferedImage. w h BufferedImage/TYPE_INT_RGB))
 
-(def fg-color-rgb (.getRGB Color/WHITE))
+(def color-rgb-fg (.getRGB Color/WHITE))
 
 (defn write-image
   [img uri]
   (with-open [file (FileOutputStream. uri)]
     (ImageIO/write img "JPG" file)))
 
-(defn getPixels
+(defn get-image-pixels
   [img]
   (vec (.getRGB img 0 0 (.getWidth img) (.getHeight img) nil 0 (.getWidth img))))
 
-(defn writeParticlesToImage
+(defn write-particles-to-image
   "Write the set of `particles` to the `image` at the given `scale`"
   [image scale particles]
   (reduce (fn [acc particle]
-            (let [start_x (* (first particle) scale)
-                  start_y (* (second particle) scale)
-                  pixel_color_arr (int-array (repeat (* scale scale) fg-color-rgb))]
-              (doto acc (.setRGB start_x start_y scale scale pixel_color_arr 0 scale))))
+            (let [start-x (* (first particle) scale)
+                  start-y (* (second particle) scale)
+                  scaled-pixel-colors (int-array (repeat (* scale scale) color-rgb-fg))]
+              (doto acc (.setRGB start-x start-y scale scale scaled-pixel-colors 0 scale))))
           image
           particles))

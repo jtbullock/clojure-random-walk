acc5aa1724c23293ce8f3e228a55492e65d8cc8f
Josh Bullock <mail@joshb.me>
2022-11-15 11:37:51 -0500

    Give function doc strings a once over

--- src/clojure_random_walk/simulation.clj
+++ src/clojure_random_walk/simulation.clj
@@ -1,42 +1,37 @@
 (ns clojure-random-walk.simulation
   (:require [clojure.set :refer [intersection]]))
 
+; Possible directions a particle can move, and test functions
+; to determine if the direction is valid given a particle's position in the simulation.
+(def particle-move-directions
+  [{:name "down"  :shift [ 1  0] :test (fn [particle sim] (not= (first particle) (dec (sim :height))))}
+   {:name "up"    :shift [-1  0] :test (fn [particle _] (not= (first particle) 0))}
+   {:name "right" :shift [ 0  1] :test (fn [particle sim] (not= (second particle) (dec (sim :width))))}
+   {:name "left"  :shift [ 0 -1] :test (fn [particle _] (not= (second particle) 0))}])
+
+(defn add-fixed-particle
+  "Add fixed particle to `simulation` at coordinates `[x y]`."
+  [simulation [x y]]
+  (update simulation :fixed-particles conj [x y]))
+
+(defn add-fixed-particles
+  "Add fixed `particles` ([x y] coordinates) to `simulation`."
+  [simulation & particles]
+  (reduce add-fixed-particle simulation particles))
+
 (defn create-simulation
   "Create a new simulation with the specified `width` and `height`."
   [width height]
   {:width width, :height height, :fixed-particles #{}})
 
-(defn add-fixed-particle
-  "Add a fixed point to a `simulation` at coordinates `x` and `y`."
-  [simulation [x y]]
-  (update simulation :fixed-particles conj [x y]))
-
-(defn add-fixed-particles
-  "Add fixed `points` to `simulation`"
-  [simulation & points]
-  (reduce add-fixed-particle simulation points))
-
 (defn get-adjacent-points
-  "Return a set of points adjacent to the provided `[x y]` coordinates."
+  "Return a lazy sequence of points adjacent to the provided `[x y]` coordinates."
   [[x y]]
   (->> #{[1 0] [-1 0] [0 1] [0 -1]}
        (map (fn [[x2 y2]] [(+ x x2) (+ y y2)])) set))
 
-(defn points-adjacent?
-  "Determine if coordinates `[x y]` are adjacent to a fixed point
-   with coordinates `[fixed-x fixed-y]`."
-  [fixed-particle particle]
-  (contains? (get-adjacent-points particle) fixed-particle))
-
-(defn simulation-contains-adjacent-point?
-  "Determine if simulation contains a fixed point adjacent to
-   the `[x y]` coordinates."
-  [simulation point]
-  (->> (get-adjacent-points point)
-       (intersection (simulation :fixed-particles)) seq boolean))
-
 (defn get-all-boundary-points
-  "Return a list of all points on boundaries of simulation."
+  "Return a lazy seq of all points on boundary of simulation."
   [simulation]
   (let [sim-width (simulation :width)
         sim-height (simulation :height)
@@ -54,29 +49,33 @@
   [simulation]
   (rand-nth (get-all-boundary-points simulation)))
 
-(def particle-move-directions
-  [{:name "down" :shift [1 0] :test (fn [particle sim] (not= (first particle) (dec (sim :height))))}
-   {:name "up" :shift [-1 0] :test (fn [particle _] (not= (first particle) 0))}
-   {:name "right" :shift [0 1] :test (fn [particle sim] (not= (second particle) (dec (sim :width))))}
-   {:name "left" :shift [0 -1] :test (fn [particle _] (not= (second particle) 0))}])
-
 (defn move-particle
-  "Move `particle` in a random direction within the `simulation`",
+  "Move `particle` in a random direction within the `simulation`.",
   [simulation particle]
   (let [possible-directions (filter #((:test %) particle simulation) particle-move-directions)
         random-direction-shift (:shift (rand-nth possible-directions))]
     (mapv + particle random-direction-shift)))
 
+(defn particles-adjacent?
+  "Determine if two particles (in [x y] format) are adjacent to each other."
+  [particle-1 particle-2]
+  (contains? (get-adjacent-points particle-1) particle-2))
+
+(defn simulation-contains-adjacent-fixed-particle?
+  "Determine if `simulation` contains a fixed particle adjacent to the `[x y]` coordinates."
+  [simulation point]
+  (->> (get-adjacent-points point)
+       (intersection (simulation :fixed-particles)) seq boolean))
+
 (defn walk-particle
-  "Randomly walk `particle` around `simulation` until it wanders next to
-  a fixed particle."
+  "Randomly walk `particle` around `simulation` until it wanders next to a fixed particle."
   [particle simulation]
   (->> (iterate (partial move-particle simulation) particle)
-       (drop-while #(not (simulation-contains-adjacent-point? simulation %)))
+       (drop-while #(not (simulation-contains-adjacent-fixed-particle? simulation %)))
        (first)))
 
 (defn run-simulation
-  "Run a simulation!"
+  "Run a preset simulation! Temporary until command line args are set up."
   []
   (let [simulation (add-fixed-particle (create-simulation 100 100) [49 49])]
     (reduce (fn [acc, _] (add-fixed-particle acc (walk-particle (get-random-boundary-point acc) acc)))

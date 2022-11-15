b6597862da6e770695f0ebee36c4065d8f39295e
Josh Bullock <mail@joshb.me>
2022-11-15 10:54:17 -0500

    Remove unneeded code

--- src/clojure_random_walk/image_output.clj
+++ src/clojure_random_walk/image_output.clj
@@ -8,13 +8,6 @@
 
 (def fg-color-rgb (.getRGB Color/WHITE))
 
-(defn draw-square [img color]
-  (doto img
-    (.setRGB 20 20 color)
-    (.setRGB 20 21 color)
-    (.setRGB 21 20 color)
-    (.setRGB 21 21 color)))
-
 (defn write-image
   [img uri]
   (with-open [file (FileOutputStream. uri)]
@@ -23,26 +16,6 @@
 (defn getPixels
   [img]
   (vec (.getRGB img 0 0 (.getWidth img) (.getHeight img) nil 0 (.getWidth img))))
-
-(defn getRGBTest
-  []
-  (-> (create-image 3 3)
-      (doto
-        (.setRGB 1 1 fg-color-rgb))
-      (getPixels)))
-
-(defn getPixelScalingMatrix
-  "Given a `scale` size, return a matrix for scaling a pixel."
-  [scale]
-  (set (for [x (range 0 scale)
-             y (range 0 scale)] [x y])))
-
-(defn particle->scaled_pixels
-  "Given a `particle` and a `scale`, return an array of pixels."
-  [[p_x p_y] scale]
-  (->> (getPixelScalingMatrix scale)
-       (map (fn [[m_x m_y]] [(+ p_x m_x) (+ p_y m_y)]))
-       set))
 
 (defn writeParticlesToImage
   "Write the set of `particles` to the `image` at the given `scale`"
@@ -54,5 +27,3 @@
               (doto acc (.setRGB start_x start_y scale scale pixel_color_arr 0 scale))))
           image
           particles))
-
-;(writeParticlesToImage img 3 sample-particles)
--- test/clojure_random_walk/image_output_test.clj
+++ test/clojure_random_walk/image_output_test.clj
@@ -2,21 +2,6 @@
   (:require [clojure.test :refer :all]
             [clojure-random-walk.image-output :refer :all]))
 
-(deftest getPixelScalingMatrix-test
-  (testing "Creates a matrix for scale 1"
-    (is (= #{[0 0]} (getPixelScalingMatrix 1))))
-  (testing "Creates a matrix for scale 3"
-    (is (= #{[0 0] [0 1] [0 2]
-             [1 0] [1 1] [1 2]
-             [2 0] [2 1] [2 2]} (getPixelScalingMatrix 3)))))
-
-(deftest particle->scaled_pixels-test
-  (testing "Converts a particle to pixels with scale of 1."
-    (is (= #{[5 5]} (particle->scaled_pixels [5 5] 1))))
-  (testing "Converts a particle to pixels with scale of 2."
-    (is (= #{[5 5] [5 6]
-             [6 5] [6 6]} (particle->scaled_pixels [5 5] 2)))))
-
 (deftest writeParticlesToImage-test
   (let [particles #{[1 1] [0 1] [1 0]}
         black -16777216 white -1]
